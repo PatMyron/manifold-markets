fac7da29f2923194cc3ed5afc072399cf8de249d
Pat Myron <PatMyron@users.noreply.github.com>
2023-02-07 19:10:30 -0500

    filter MULTIPLE_CHOICE / FREE_RESPONSE

--- extreme.py
+++ extreme.py
@@ -7,5 +7,7 @@ for bet in response:
   if bet['probAfter'] > .99 or bet['probAfter'] < .01:
     bets.add('https://manifold.markets/api/v0/market/' + bet['contractId'])
 for bet in bets:
-  markets.append(requests.get(bet).json())
+  response = requests.get(bet).json()
+  if response['outcomeType'] != "MULTIPLE_CHOICE" and response['outcomeType'] != "FREE_RESPONSE":
+    markets.append(response)
 print(json.dumps(markets, indent=1))
