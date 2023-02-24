2853f8def53dc4504542fddd85d30edf8f5a72c7
Pat Myron <pmyron@nd.edu>
2023-02-24 11:09:40 -0500

    future open markets

--- src/open.py
+++ src/open.py
@@ -1,11 +1,12 @@
 import json
 import requests
+import time
 url = 'https://manifold.markets/api/v0/markets?limit=1000'
 response = requests.get(url).json()
 output = []
 while response:
   for market in response:
-    if not market['isResolved']:
+    if not market['isResolved'] and market['closeTime']/1000 > time.time():
       output.append(market)
   response = requests.get(url + '&before=' + response[-1]['id']).json()
 print(json.dumps(output, indent=1))
