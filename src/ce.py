5ef045e386a6586a33d01a880d36b2e1846583dd
Pat Myron <pmyron@nd.edu>
2023-06-01 17:20:59 -0400

    CE contest

---
+++ src/ce.py
@@ -0,0 +1,14 @@
+import requests
+from statistics import mean 
+overall = []
+paw = []
+media = []
+for market in requests.get('https://manifold.markets/api/v0/group/by-id/mGCdPei3RXTEoq1IGOAC/markets').json():
+  overall.append(market['probability'])
+  if market['question'].startswith('PAW'):
+    paw.append(market['probability'])
+  else:
+    media.append(market['probability'])
+print(mean(overall))
+print('paw', mean(paw))
+print('media', mean(media))
