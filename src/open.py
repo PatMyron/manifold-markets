087408104a7a77d65a18b0b5d31e8429a69ab521
Pat Myron <pmyron@nd.edu>
2024-01-25 16:07:53 -0500

    default closeTime value to avoid KeyError

--- src/open.py
+++ src/open.py
@@ -6,7 +6,7 @@ response = requests.get(url).json()
 output = []
 while response:
   for market in response:
-    if not market['isResolved'] and market['closeTime']/1000 > time.time():
+    if not market['isResolved'] and market.get('closeTime', 0)/1000 > time.time():
       output.append(market)
   response = requests.get(url + '&before=' + response[-1]['id']).json()
 print(json.dumps(output, indent=1))
--- src/soon.py
+++ src/soon.py
@@ -6,7 +6,7 @@ response = requests.get(url).json()
 output = []
 while response:
   for market in response:
-    if not market['isResolved'] and market['closeTime']/1000 < time.time() + 2*30*24*60*60 and market['closeTime']/1000 > time.time():
+    if not market['isResolved'] and market.get('closeTime', 0)/1000 > time.time() and market.get('closeTime', 0)/1000 < time.time() + 2*30*24*60*60:
       output.append(market)
   response = requests.get(url + '&before=' + response[-1]['id']).json()
 print(json.dumps(output, indent=1))
